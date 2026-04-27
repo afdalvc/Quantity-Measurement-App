2aed8af6ae0c82d6d6cc4632c0a8156ebed5765e
afdalvc <afdalr903@gmail.com>
2026-04-27 09:38:48 +0530

    UC1: Feet measurement equality

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,21 @@
-public class TrainConsistManagementApp {
+package QuantityMeasurementApp;
+class QuantityMeasurementApp {
+    static class Feet {
+        private final double value;
+        public Feet(double value) {
+            this.value = value;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true; // same reference
+            if (obj == null || getClass() != obj.getClass()) return false; // null/type check
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0; // value comparison
+        }
+    }
+    public static void main(String[] args) {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(2.0);
+        System.out.println("Equal (" + f1.equals(f2) + ")");
+    }
 }
